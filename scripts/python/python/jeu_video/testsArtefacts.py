e4998470ef705d4f25960fc415509e55dee568b5
Overclock9gh <59988450+Overclock9gh@users.noreply.github.com>
2021-02-12 18:14:54 -0500

    Add files via upload

---
+++ scripts/python/python/jeu_video/testsArtefacts.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+# -*-coding:Utf-8 -*
+import os 
+from pkg_artefacts.mod_artefacts import *
+
+# test de la fonction posseseurs_armes
+artefact_selon_element()
+# test de la fonction affinite_heros
+artefacts_avec_pouvoir_special()
---
+++ scripts/python/python/jeu_video/testsHeros.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+# -*-coding:Utf-8 -*
+import os 
+from pkg_heros.mod_heros import *
+
+# test de la fonction posseseurs_armes
+attaque_heros()
+# test de la fonction affinite_heros
+heros_avec_affinite()
